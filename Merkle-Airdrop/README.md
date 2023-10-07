7a93cf64033d4e41f1bcdde36090a8c9c784f05d
Esther Breath <estherojile248@gmail.com>
2023-10-07 17:58:35 +0100

    first commit

---
+++ Merkle-Airdrop/README.md
@@ -0,0 +1,9 @@
+
+This repository contains a smart contract that implements a Merkle-tree for distributing airdrops to various addresses based on a predetermined whitelist.
+
+Overview
+The contract hashes the address and the respective airdrop amount for each eligible address into a Merkle root. During the distribution process, claimers provide an address which is then confirmed as a leaf in the Merkle root, allowing them to claim their airdrop.
+
+### Test
+
+The contract has been tested using Foundry, ensuring its functionality and validity.
---
+++ Merkle-Airdrop/src/Airdrop.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+// import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "solmate/src/utils/MerkleProofLib.sol";
+
+contract Airdrop is ERC20("MProof", "MPT") {
+    bytes32 merkleRoot;
+
+    constructor(bytes32 _root) {
+        merkleRoot = _root;
+    }
+
+    mapping(address => bool) public hasClaimed;
+    event AddressClaim(address account, uint256 amount);
+
+    function claim(
+        bytes32[] calldata _merkleProof,
+        address claimer,
+        uint256 _amount
+    ) external returns (bool success) {
+        require(!hasClaimed[claimer], "You have already claimed!");
+
+        bytes32 node = keccak256(abi.encodePacked(claimer, _amount));
+
+        success = MerkleProofLib.verify(_merkleProof, merkleRoot, node);
+        require(success, "Invalid Verification");
+        hasClaimed[claimer] = true;
+        _mint(claimer, _amount);
+        emit AddressClaim(claimer, _amount);
+    }
+}
